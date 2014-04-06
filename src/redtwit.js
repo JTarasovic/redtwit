817b7f52756db0f21fc95bdc6875f564ee8afb39
Jason Tarasovic <jason@tarasovic.com>
2014-04-06 15:30:31 -0500

    redis connection ongoing - can pull list of subreddits from LIST. Cannot retrieve last update time :(

--- src/redtwit.js
+++ src/redtwit.js
@@ -6,14 +6,25 @@ var log = require("console").log;
 var ts = Date.now() / 1000;
 var offset = 90*60;
 ts = ts - offset;
-var subreddits = ["android", "cypherpunks", "linux"];
 
+getSubreddits = function(err,items) {
+	if (err) {throw error;};
+	items.forEach(getNewSubmissions);
+}
+
+getNewSubmissions = function(sub) {
+	request('http://www.reddit.com/r/'+sub+'/new.json?sort=new', processSubmissions);
+}
 
 processSubmissions = function(err,resp,body){
 	if (!err && resp.statusCode == 200) {
 		var newSubmissions = JSON.parse(body);
 		newSubmissions = newSubmissions.data.children;
 		
+		if (newSubmissions[0].data.subreddit) {
+			checkTime(newSubmissions[0].data.subreddit)
+		};
+
 		for(var i=0; i < newSubmissions.length; i++) {
 			if (newSubmissions[i].data.created_utc > ts) {
 				log(newSubmissions[i].data.title + "\n");
@@ -22,10 +33,15 @@ processSubmissions = function(err,resp,body){
 	}
 };
 
-getNewSubmissions = function(sub) {
-	request('http://www.reddit.com/r/'+sub+'/new.json?sort=new', processSubmissions})
+checkTime = function(sub) {
+	sub = sub.toLowerCase();
+	var query = sub + ':last.update';
+	log(query);
+	client.get(query,redis.print);
 }
 
-client.LRANGE(0,-1,redis.print);
 
-//subreddits.forEach(getNewSubmissions);
+
+client.LRANGE("subreddits", 0, -1, getSubreddits);
+
+client.quit();
