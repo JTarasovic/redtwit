7dff2b1e447542b05646d19cf44bc5c99491d2e1
Jason Tarasovic <jason@tarasovic.com>
2014-04-12 16:22:26 -0500

    added onstream end test

--- tests/twitterhandlertest.js
+++ tests/twitterhandlertest.js
@@ -19,33 +19,39 @@ function onStream (data, resp) {
 	console.error(util.inspect(arguments));
 }
 
+function onStreamEnd (data, resp) {
+	console.log('\nstream');
+	console.error('\n\ntwitterhandlertest.js > onStreamEnd > ~24');
+	console.error(util.inspect(arguments));
+}
+
 function onDM (data, resp) {
 	console.log('\ndm');
-	console.error('\n\ntwitterhandlertest.js > onDM > ~22');
+	console.error('\n\ntwitterhandlertest.js > onDM > ~30');
 	console.error(util.inspect(arguments));
 }
 
 function onFollow (data) {
 	console.log('\nfollow!');
-	console.error('\n\ntwitterhandlertest.js > onFollow > ~30');
+	console.error('\n\ntwitterhandlertest.js > onFollow > ~36');
 	console.error(util.inspect(arguments));
 }
 
 function onFavorite (data) {
 	console.log('\nfavorite');
-	console.error('\n\ntwitterhandlertest.js > onFavorite > ~36');
+	console.error('\n\ntwitterhandlertest.js > onFavorite > ~42');
 	console.error(util.inspect(arguments));
 }
 
 function onUnFavorite (data) {
 	console.log('\nunfavorite');
-	console.error('\n\ntwitterhandlertest.js > onunFavorite > ~42');
+	console.error('\n\ntwitterhandlertest.js > onunFavorite > ~48');
 	console.error(util.inspect(arguments));
 }
 
 function onError (err, resp, post) {
 	console.log('\nerror');
-	console.error('\n\ntwitterhandlertest.js > onError > ~48');
+	console.error('\n\ntwitterhandlertest.js > onError > ~54');
 	console.error(util.inspect(arguments));
 }
 
